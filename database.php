1e7c23cdfa700f2821882f8972001a5cc2f93185
TraceyMunyagia <tracey.munyagia@strathmore.edu>
2023-09-29 12:49:58 +0300

    commit

---
+++ database.php
@@ -0,0 +1,19 @@
+gi<?php
+class Database {
+    private $host = "localhost: 3306" ;
+    private $username = "root";
+    private $password = "";
+    private $database = "Register";
+    public $conn;
+
+    public function __construct() {
+        try {
+            $dsn = "mysql:host={$this->host};dbname={$this->database}";
+            $this->conn = new PDO($dsn, $this->username, $this->password);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            die("Connection failed: " . $e->getMessage());
+        }
+    }
+}
+?>
